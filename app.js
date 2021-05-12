cebefdc0bc4782210590085a8500a8cd50f3bf1a
Eliam LOTONGA <e.m.lotonga@gmail.com>
2021-05-12 23:46:02 +0200

    Indent code

--- app.js
+++ app.js
@@ -68,42 +68,41 @@ const addSearchListener = (input, callback, delay = 1200) => {
  * @returns {Promise} http response
  */
 const getUsers = (search, attribute = 'first_nameORlast_name') => {
-        return fetch(
-            `http://127.0.0.1:8000/api/v1/users/filter/${attribute}/${search}`
-        ).then((data) => {
-            return data.json().then((response) => {
-                if (response.users.length == 0) {
-                    response = getUsers(search, 'last_name')
-                    return response
-                }
-                return response.users
-            })
-        })
-    }
-    [old_owner_input, new_owner_input].forEach((input) => {
-        addSearchListener(input, () => {
-            const username = input.value.trim().toLowerCase()
-
-            if (username.length >= 3) {
-                getUsers(username).then((users) => {
-                    usersToSuggest = users.filter(
-                            (user) => user.first_name.toLowerCase().includes(username) ||
-                            user.last_name.toLowerCase().includes(username)
-                        )
-                        // clear previous suggestions before inserting new ones
-                    clearSuggestions(input.nextElementSibling)
-                    usersToSuggest.forEach((user) => {
-                        p = document.createElement('P')
-                        p.setAttribute('id', `user_${user.id}`)
-                        p.setAttribute('class', 'user_item')
-                        p.innerText = `${user.first_name} ${user.last_name}`
-                        input.nextElementSibling.appendChild(p)
-                        videos_container.appendChild(createAvatarDOM(user))
-                    })
-                })
-            } else clearSuggestions(input.nextElementSibling)
+    return fetch(
+        `http://127.0.0.1:8000/api/v1/users/filter/${attribute}/${search}`
+    ).then((data) => {
+        return data.json().then((response) => {
+            if (response.users.length == 0) {
+                response = getUsers(search, 'last_name')
+                return response
+            }
+            return response.users
         })
     })
+}
+[old_owner_input, new_owner_input].forEach((input) => {
+    addSearchListener(input, () => {
+        const username = input.value.trim().toLowerCase()
+        if (username.length >= 3) {
+            getUsers(username).then((users) => {
+                usersToSuggest = users.filter(
+                    (user) => user.first_name.toLowerCase().includes(username) ||
+                        user.last_name.toLowerCase().includes(username)
+                )
+                // clear previous suggestions before inserting new ones
+                clearSuggestions(input.nextElementSibling)
+                usersToSuggest.forEach((user) => {
+                    p = document.createElement('P')
+                    p.setAttribute('id', `user_${user.id}`)
+                    p.setAttribute('class', 'user_item')
+                    p.innerText = `${user.first_name} ${user.last_name}`
+                    input.nextElementSibling.appendChild(p)
+                    videos_container.appendChild(createAvatarDOM(user))
+                })
+            })
+        } else clearSuggestions(input.nextElementSibling)
+    })
+})
 
 suggestions.forEach((suggestion) => {
     suggestion.addEventListener('click', (e) => {
